d620e54f8c45ad1ecaea63810e62e67d9ff2ec21
mjjimenez <mjjimenez87@gmail.com>
2016-03-23 09:19:31 +0800

    Initial Commit

---
+++ Freelancer-Exam-Jimenez/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Freelancer-Exam-Jimenez
+//
+//  Created by Mark Jeremiah Jimenez on 23/03/2016.
+//  Copyright © 2016 Mark Jeremiah Jimenez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
